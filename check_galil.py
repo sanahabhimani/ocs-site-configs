84f8d746c1c5d87d45a767168acc033cf4f2caf0
sanah <sanahbhimani@gmail.com>
2025-10-22 18:26:40 -0500

    udpated clients

--- check_galil.py
+++ check_galil.py
@@ -1,23 +1,58 @@
 from ocs.ocs_client import OCSClient
 client = OCSClient('satcouplingoptics')
+#client.get_motor_type(axis='F')
 
+#client.set_motor_type(axis='F', motortype=1)
+#client.get_off_on_error(axis='F')
+#client.set_off_on_error(axis='F', errtype=1)
 
-
-#client.enable_axis(axis='E')
-
-#client.jog_axis(axis='E', speed=20000.0)
-#client.begin_axis_motion(axis='E')
-
-
-#client.set_amp_currentloop_gain(axis='E',  val=0.0)
-#client.set_torque_limit(axis='E', val=2.0)
-#client.disable_axis(axis='E')
-#client.disable_axis(axis='F')
+#client.get_amp_gain(axis='E')
 #client.set_amp_gain(axis='E', val=0)
-#client.set_off_on_error(axis='E', errtype=1)
-#client.set_motortype(axis='E', motortype=1)
-#time.sleep(1)
-#client.acq.start()
+#client.get_amp_gain(axis='E')
+
+
+#client.get_amp_gain(axis='F')
+#client.set_amp_gain(axis='F', val=0)
+#client.get_amp_gain(axis='F')
+
+
+
+
+
+#client.set_torque_limit(axis='F', val=2.0)
+
+#client.set_torque_limit(axis='F', val=2.0)
+
+
+
+#client.get_amp_currentloop_gain(axis='E')
+#client.set_amp_currentloop_gain(axis='E',  val=0)
+#client.get_amp_currentloop_gain(axis='E')
+
+
+#client.get_amp_currentloop_gain(axis='F')
+#client.set_amp_currentloop_gain(axis='F',  val=0)
+#client.get_amp_currentloop_gain(axis='F')
+
+
+#client.set_gearing(order=',,,,,E;')
+#client.set_gearing_ratio(order=',,,,,1;')
+
+#client.get_gearing_ratio(axis='E')
+#client.get_gearing_ratio(axis='F')
+
+# now enable the motor
+#client.get_motor_state(axis='F')
+#client.set_motor_state(axis='F', state='enable')
+#client.get_motor_state(axis='F')
+
+
+
+
+client.jog_axis(axis='E', speed=8000.0)
+client.begin_axis_motion(axis='E')
+
+
 
 
 
